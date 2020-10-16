1b0179f67ad7b1fe4eb1ffcafe68d19947c46a85
Oleg Masyagin <olegmasyagin@gmail.com>
2020-10-16 19:43:15 +0300

    Добавил и подключил style.min.css и script.min.js slider.min.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const link = document.querySelector('.open-modal-button');
-const popup = document.querySelector('.modal');
+const popup = document.querySelector('.modal-contact');
 const close = popup.querySelector('.modal-close');
 const form = popup.querySelector('form');
 const nameField = popup.querySelector('[name=name]');
@@ -22,7 +22,6 @@ link.addEventListener('click', function(evt) {
   } else {
     nameField.focus();
   }
-
 });
 
 close.addEventListener('click', function(evt) {
@@ -53,41 +52,3 @@ window.addEventListener('keydown', function(evt) {
   }
 });
 
-const slide1 = document.querySelector('.slide:nth-child(1)');
-const slide2 = document.querySelector('.slide:nth-child(2)');
-const slide3 = document.querySelector('.slide:nth-child(3)');
-
-const sliderControl = document.querySelector('.slider-control');
-const button1 = sliderControl.querySelector('button:nth-child(1)');
-const button2 = sliderControl.querySelector('button:nth-child(2)');
-const button3 = sliderControl.querySelector('button:nth-child(3)');
-
-button1.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  button1.classList.add('current');
-  button2.classList.remove('current');
-  button3.classList.remove('current');
-  slide1.classList.add('slide-current');
-  slide2.classList.remove('slider-current');
-  slide3.classList.remove('slide-current');
-});
-
-button2.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  button2.classList.add('current');
-  button1.classList.remove('current');
-  button3.classList.remove('current');
-  slide2.classList.add('slide-current');
-  slide1.classList.remove('slide-current');
-  slide3.classList.remove('slide-current');
-});
-
-button3.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  button3.classList.add('current');
-  button1.classList.remove('current');
-  button2.classList.remove('current');
-  slide3.classList.add('slide-current');
-  slide1.classList.remove('slide-current');
-  slide2.classList.remove('slide-current');
-})
